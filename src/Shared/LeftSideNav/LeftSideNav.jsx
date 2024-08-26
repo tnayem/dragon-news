5600b319e6cbf8abacc409bd7a2ac603041ae9ab
tnayem <tasdidnayem@gmail.com>
2024-08-26 19:59:23 +0600

    implement header section

---
+++ src/Shared/LeftSideNav/LeftSideNav.jsx
@@ -0,0 +1,9 @@
+const LeftSideNav = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Left Side Nav</h2>
+        </div>
+    );
+};
+
+export default LeftSideNav;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import Header from "../../Shared/Header/Header";
+import LeftSideNav from "../../Shared/LeftSideNav/LeftSideNav";
+import Navbar from "../../Shared/Navbar/Navbar";
+import RightSideNav from "../../Shared/RightSideNav/RightSideNav";
+
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h1 className="font-poppins">This is Home</h1>
+            <div className="grid grid-cols-1 md:grid-cols-4 gap-6"> 
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="border md:col-span-2">
+                    <h2 className="text-3xl">News Comming Soon</h2>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
